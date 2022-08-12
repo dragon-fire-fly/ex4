4810eb4c57caaea55bc7c06719dc09296a954b1c
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-12 09:52:53 +0200

    Add movie task

--- exercise.py
+++ exercise.py
@@ -134,3 +134,32 @@ for indexnum, month in enumerate(months): # enumerate gives the index number (ne
 
 
 print(f"{user_month} has {days[indexnum]} days")
+
+
+#---- Movie Task ----#
+import random
+import os
+
+movie_list = ["1.mp4", 
+"2.mp4", 
+"3.mp4", 
+"4.mp4", 
+"5.mp4", 
+"6.mp4", 
+"7.mp4", 
+"8.mp4", 
+"9.mp4", 
+"10.mp4", 
+"11.mp4", 
+"12.mp4", 
+"13.mp4", 
+"14.mp4", 
+"15.mp4", 
+"16.mp4", 
+"17.mp4", ]
+
+choice = random.randint(0,16)
+
+print(movie_list[choice])
+# os.startfile(movie_list[choice])
+
